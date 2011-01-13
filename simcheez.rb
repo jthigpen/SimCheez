5d4d6d47183872637875d9650f877d0c39f030eb
James Thigpen <james.r.thigpen@gmail.com>
2011-01-12 22:04:16 -0800

    Fix lib loading.

--- simcheez.rb
+++ simcheez.rb
@@ -1,47 +1,46 @@
-require 'amqp'
-require 'mq'
+  require 'amqp'
+  require 'mq'
 
-class AssetMessage
-  attr_reader :asset_id
-  attr_reader :type
-end
-
-class AssetCreatedMessage < AssetMessage
-  def initialize(asset_id)
-    @asset_id = asset_id
-    @type = :asset_created
-  end
-end
-
-class UploadAssetMessage < AssetMessage
-  def initialize
-    @asset_id = Random.new.rand(1..10000)
-    @type = :asset_upload
-  end
-end
-
-class MessageHandler 
-  def handle(msg)
-    puts "Handling #{msg}"
-    self.send(msg.type, msg) if self.respond_to? msg.type
+  class AssetMessage
+    attr_reader :asset_id
+    attr_reader :type
   end
 
-  def start_consuming_messages
-    AMQP.start(:host => 'localhost' ) do
-      @q = MQ.new.queue('tasks')
-      @q.subscribe do |msg|
-        begin
-          marshalled_message = Marshal.load(msg)
-          self.handle(marshalled_message)
-        rescue
-          puts "Oh shit: #{msg}"
-        end
-     end
+  class AssetCreatedMessage < AssetMessage
+    def initialize(asset_id)
+      @asset_id = asset_id
+      @type = :asset_created
     end
   end
-  
-  def publish(message)
-    @q.publish(message)
-  end
-end
 
+  class UploadedAssetMessage < AssetMessage
+    def initialize
+      @asset_id = Random.new.rand(1..10000)
+      @type = :asset_upload
+    end
+  end
+
+  class MessageHandler 
+    def handle(msg)
+      puts "Handling #{msg}"
+      self.send(msg.type, msg) if self.respond_to? msg.type
+    end
+
+    def start_consuming_messages
+      AMQP.start(:host => 'localhost' ) do
+        @q = MQ.new.queue('tasks')
+        @q.subscribe do |msg|
+          begin
+            marshalled_message = Marshal.load(msg)
+            self.handle(marshalled_message)
+          rescue
+            puts "Oh shit: #{msg}"
+          end
+       end
+      end
+    end
+    
+    def publish(message)
+      @q.publish(message)
+    end
+  end
